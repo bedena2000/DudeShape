dd136b24c48f447f9139847286a11bbf283266ad
bedena2000 <bedena2000@gmail.com>
2022-04-10 21:57:47 +0400

    first commit

---
+++ dist/script/app.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.furniture-wrapper-box-right-right-arrow',
+    prevEl: '.furniture-wrapper-box-right-left-arrow',
+  },
+
+  
+});
